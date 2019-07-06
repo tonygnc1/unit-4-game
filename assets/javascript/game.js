231663ebc4c6fc64a05790ab4660cd7e19a2436e
tonygnc1 <bootcamp1955@gmail.com>
2019-07-06 11:53:19 -0400

    Update background image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,11 +10,11 @@ var losses = "0";
 
 
 function startNewGame() {
-var min=19; 
-    var max=120;  
-    var random =Math.floor(Math.random() * (+max - +min)) + +min; 
+    var min = 19;
+    var max = 120;
+    var random = Math.floor(Math.random() * (+max - +min)) + +min;
     // document.write("Random Number Generated : " + random );  
-console.log(random);
+    console.log(random);
 }
 
 //     function getRandomIntInclusive(min, max) {
@@ -22,8 +22,8 @@ console.log(random);
 //         max = Math.floor(120);
 //         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 //       }
-    // computerNumber = Math.floor(Math.random() * 19-120 +1);
-    
+// computerNumber = Math.floor(Math.random() * 19-120 +1);
+
 // }
 
 startNewGame()
