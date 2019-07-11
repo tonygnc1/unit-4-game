3f81558af1dfd7f542d8e11e232b451b7009ec90
tonygnc1 <bootcamp1955@gmail.com>
2019-07-11 16:57:42 -0400

    added updated with crystal btn

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,33 @@
-var computerNumber;
-var crystal1;
-var crystal2;
-var crystal3;
-var crystal4;
+var randomNumber = 0;
 var scoreBoard;
 var userScore;
-var wins = "0";
-var losses = "0";
+var wins = 0;
+var losses = 0;
+
+var crystalValues = {
+    crystal1: 0,
+    crystal2: 0,
+    crystal3: 0,
+    crystal4: 0,
+};
 
 
-function startNewGame() {
-    var min = 19;
-    var max = 120;
-    var random = Math.floor(Math.random() * (+max - +min)) + +min;
-    // document.write("Random Number Generated : " + random );  
-    console.log(random);
+function reset() {
+    randomNumber = Math.floor(Math.random() * 102 + 19);
+    console.log(randomNumber);
+    $("#randomNumber").text(randomNumber)
+    
+    crystalValues.crystal1 = Math.floor(Math.random() * 12 + 1);
+    crystalValues.crystal2 = Math.floor(Math.random() * 12 + 1);
+    crystalValues.crystal3 = Math.floor(Math.random() * 12 + 1);
+    crystalValues.crystal4 = Math.floor(Math.random() * 12 + 1);
+    console.log(crystalValues)
 }
 
-//     function getRandomIntInclusive(min, max) {
-//         min = Math.ceil(19);
-//         max = Math.floor(120);
-//         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-//       }
-// computerNumber = Math.floor(Math.random() * 19-120 +1);
+reset();
 
-// }
-
-startNewGame()
-
-
-// $(document).ready(function startGame() {
-//     var randomNumber = Math.floor(Math.random() * 100+21);
-//     $("#numberToGet").text(randomNumber);
-//     alert("randomNumber");
-// })
-
-// var min=19; 
-//     var max=120;  
-//     var random =Math.floor(Math.random() * (+max - +min)) + +min;
-//     document.write("Random Number Generated : " + random );     
-    // alert(random);
-
-// startGame();
-
+function popCrystal(evt, getCrystal) {
+    document.getElementById(getCrystal).style.float = "right";
+}
 
 
